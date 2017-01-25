d78846a5f9217a0de6a05fee4710ae92e8d17944
Teembokeem <myungtkim@gmail.com>
2017-01-25 14:55:54 -0800

    add ngmap init, and google maps positioning for a to b routes

--- www/views/currentOrder/currentOrder.js
+++ www/views/currentOrder/currentOrder.js
@@ -3,7 +3,7 @@
 
     var dir = 'views';
     angular
-        .module('currentOrder', ['cancelOrderButton', 'cancelOrder', 'scanner', 'verifyCard', 'signature', 'completedOrder'])
+        .module('currentOrder', ['deliveryFail', 'cancelOrder', 'scanner', 'verifyCard', 'signature', 'completedOrder'])
         .config(routes)
         .controller('currentOrderCrtl', currentOrderCrtl);
 
@@ -19,27 +19,71 @@
             });
     }
 
-    currentOrderCrtl.$inject = ['$state', 'Order'];
-    function currentOrderCrtl($state, Order) {
+    currentOrderCrtl.$inject = ['$state', 'Order', '$ionicPopup', 'NgMap', '$cordovaGeolocation', '$stateParams', 'stateManager'];
+    function currentOrderCrtl($state, Order, $ionicPopup, NgMap, $cordovaGeolocation, $stateParams, state) {
         var $ctrl = this;
         $ctrl.goToOrders = goToOrders;
         $ctrl.startScan = startScan;
 
         activate();
 
+
+
+        $ctrl.showNote = false;
+
+        $ctrl.toggleNote = function() {
+            $ctrl.showNote = !$ctrl.showNote;
+        };
+
         ////////////////
 
         function activate() {
+            if (!Order.current) {
+                return state.goNoBack('app.orders');
+            }
             $ctrl.currentOrder = Order.current;
-        }
+            console.log($ctrl.currentOrder);
+
+            NgMap.getMap().then(function(map) {
+                $ctrl.map = map;
+                $ctrl.dest = [$ctrl.currentOrder.address.loc[1], $ctrl.currentOrder.address.loc[0]];
+                console.log("dest?", $ctrl.dest)
+
+                new google.maps.Marker({
+                position: new google.maps.LatLng($ctrl.currentOrder.address.loc[1], $ctrl.currentOrder.address.loc[0]),
+                map: $ctrl.map,
+                draggable: false,
+                animation: google.maps.Animation.DROP
+                });
+            });
+
+            var posOptions = {timeout: 10000, enableHighAccuracy: false};
+            $cordovaGeolocation
+                .getCurrentPosition(posOptions)
+                .then(function (position) {
+                    console.log("lat longs", $stateParams)
+                var lat  = position.coords.latitude
+                var long = position.coords.longitude
+                console.log("lats, longs, ", lat, long)
+                $ctrl.curr = [lat, long];
+                new google.maps.Marker({
+                    position: new google.maps.LatLng(lat, long),
+                    map: $ctrl.map,
+                    draggable: false,
+                    animation: google.maps.Animation.DROP
+                });
+                }, function(err) {
+                // error
+                });
+        };
 
         function goToOrders() {
             $state.go('app.orders');
-        }
+        };
 
         function startScan() {
-            console.log('Scanning')
+            console.log('$ctrlanning')
             $state.go('app.scanner');
-        }
+        };
     }
 })();
