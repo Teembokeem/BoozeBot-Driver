6d2beed2165c1c616d1968d12e177688f7733245
Teembokeem <myungtkim@gmail.com>
2017-02-02 14:48:26 -0800

    add cloudinary header, and general http inteceptor delegator

---
+++ www/core/Configs/cloudinaryHeaders.service.js
@@ -0,0 +1,34 @@
+(function() {
+'use strict';
+
+    angular
+        .module('Configs')
+        .factory('cloudinaryHeadersService', cloudinaryHeadersService);
+
+    cloudinaryHeadersService.$inject = [];
+    function cloudinaryHeadersService() {
+        var service = {
+            request: checkHeaders
+        };
+        
+        return service;
+
+        ////////////////
+        function checkHeaders(config) {
+            console.log('cloudz');
+            // Check if going to cloudinary
+            if (config.url.indexOf('api.cloudinary') >= 0 ) {
+                // Remove headers for cors
+                config.headers['Content-Type'] = undefined;
+                config.headers['X-Requested-With'] = 'XMLHttpRequest';
+                delete config.headers['Authorization']; 
+            }
+
+            if (config.url.indexOf('maps.googleapis.com/maps') >= 0) {
+                delete config.headers['Authorization']; 
+                //do something else
+            }
+            return config;
+        }
+    }
+})();
