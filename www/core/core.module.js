d5af5ae8fd7ceb515890286593aae3e0ddee1f30
ajchan11 <ajchan11@gmail.com>
2017-04-26 16:25:05 -0700

    driver side commits

--- www/core/core.module.js
+++ www/core/core.module.js
@@ -13,8 +13,8 @@
     ])
         .constant('url', function() {
             // var base = 'http://localhost';
-            var base = 'http://192.168.0.16:3000';
-            // var base = 'http://service.jasonsliquor.com';
+            // var base = 'http://192.168.0.16:3000';
+            var base = 'http://service.jasonsliquor.com';
 
             return {
                 base: base,
